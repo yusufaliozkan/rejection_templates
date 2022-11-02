5daa57104ad40454324950d76a76cd8c56fa46cf
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-02 22:13:23 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -22,12 +22,12 @@ df_new = df.sort_values(by='rejection reason')
 
 st.write('Select a rejection reason from the dropdown menu and copy the HTML template to clipboard.')
 clist = df_new['rejection reason'].unique()
+reason = st.selectbox("Select a reason:",clist)
 
 col1, col2 = st.columns(2)
 
 with col1:
-    reason = st.selectbox("Select a reason:",clist)
-
+    
     df_reason = df.loc[df_new['rejection reason']==reason, 'rejection template'].values[0]
     text_to_be_copied = df_reason
     copy_dict = {"content": text_to_be_copied}
