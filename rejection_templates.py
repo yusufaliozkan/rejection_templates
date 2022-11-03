bfb5a9be37a5d7f6ae4c2b9fcc65f964596659ce
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-03 16:04:04 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -63,7 +63,6 @@ with col1:
         st.write(df_eg3)
         st.write(df_eg4)
 
-
 with col2:
     with st.expander('Template view (' + reason+')', expanded=False):
         components.html(df_reason, height=1500)
