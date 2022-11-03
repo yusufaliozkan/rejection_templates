b71578d43f87cea3a9d702f463d40338ec17ff8f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-03 14:39:45 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -85,10 +85,12 @@ df_am2
 clist = df_am2['Publisher'].unique()
 publisher = st.selectbox("Select a publisher:",clist)
 df_eg1 = df_am2.loc[df_am2['Publisher']==publisher, 'Link'].values[0]
+df_eg2 = df_am2.loc[df_am2['Publisher']==publisher, 'Example'].values[0]
+df_eg3 = df_am2.loc[df_am2['Publisher']==publisher, 'Example Image'].values[0]
+df_eg4 = df_am2.loc[df_am2['Publisher']==publisher, 'Example File/2nd Image'].values[0]
 
 
-
-st.code(df_eg1) #, df_eg2,df_eg3, df_eg4)
+st.code(df_eg1 + df_eg2 + df_eg3 + df_eg4)
 
 
 # with col1:
