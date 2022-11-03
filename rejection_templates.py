fe86d7e64b0f95bc71876c5c1b0f218d8cc4848a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-03 14:45:40 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -90,7 +90,7 @@ df_eg3 = df_am2.loc[df_am2['Publisher']==publisher, 'Example Image'].values[0]
 df_eg4 = df_am2.loc[df_am2['Publisher']==publisher, 'Example File/2nd Image'].values[0]
 
 
-st.write([df_eg1, df_eg2, df_eg3, df_eg4])
+st.write(df_eg1, df_eg2, df_eg3, df_eg4)
 
 
 # with col1:
