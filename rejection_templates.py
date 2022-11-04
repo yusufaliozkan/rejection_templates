11f84c87484a9e117f8ea461320e4153b185a34d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-04 11:53:42 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -60,7 +60,7 @@ with col1:
         df_eg3 = df_am2.loc[df_am2['Publisher']==publisher, 'Example Image'].values[0]
         df_eg4 = df_am2.loc[df_am2['Publisher']==publisher, 'Example File/2nd Image'].values[0]
 
-        d = {'examples':[df_eg1,dfeg2,df_eg3,df_eg4]}
+        d = {'examples':[df_eg1,df_eg2,df_eg3,df_eg4]}
         df_egs = pd.DataFrame(data=d)
         df_egs
         
