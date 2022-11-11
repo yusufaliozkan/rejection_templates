a1e29b04400d149e14e28b459b642ecb85f75557
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-11 17:23:32 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -13,7 +13,7 @@ path='https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Logo_for_Imperial_Col
 st.image(path, width=200)
 st.markdown("# Rejection templates tool")
 
-st.sidebar.markdown("# Rejection templates tool") 
+# st.sidebar.markdown("# Rejection templates tool") 
 
 # Exporting dataset
 column_names = ['rejection reason', 'rejection template']
