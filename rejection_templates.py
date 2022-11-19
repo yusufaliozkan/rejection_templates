c525191bdea49a4daf63350a665e009c8aa1b721
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-19 22:18:22 +0000

    commit

--- rejection_templates.py
+++ rejection_templates.py
@@ -75,7 +75,7 @@ with tab1:
             st.code(df_reason)
 
     st.subheader('Frequently used templates')
-    col1, col2 = st.columns([2,3])
+    col1, col2 = st.columns([1,2])
 
     with col1:
         
