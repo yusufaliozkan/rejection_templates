ee5576126b8c2bae0af52b1a03347119c29305d1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-22 14:15:44 +0100

    sd

--- pages/test.py
+++ pages/test.py
@@ -111,9 +111,10 @@ with tab1:
     with col1:
         df_frequent = df_new.loc[df_new['rejection reason'].isin(['Wrong version - post-April 2016', 'Free to access link', 'OAL - arXiv', 'Duplicate record', 'Blank template'])]
         frequently = st.radio('Choose a rejection reason to display the statement', df_frequent['rejection reason'])
-        text_to_be_copied = df.loc[df_new['rejection reason']==frequently, 'rejection template'].values[0]
+        text_to_be_copied = df_new.loc[df_new['rejection reason']==frequently, 'rejection template'].values[0]
+        text_to_be_copied_plain = df_new.loc[df_new['rejection reason']==frequently, 'rejection template plain text'].values[0]
         copy_dict = {"content": text_to_be_copied}
-        df_frequent
+        copy_dict_plain = {"content": text_to_be_copied_plain}
 
         copy_button = Button(label="Copy HTML template to clipboard")
         copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
@@ -128,6 +129,18 @@ with tab1:
             override_height=75,
             debounce_time=0)
 
+        copy_button = Button(label="Copy plain text template to clipboard")
+        copy_button.js_on_event("button_click", CustomJS(args=copy_dict_plain, code="""
+            navigator.clipboard.writeText(content);
+            """))
+
+        no_event = streamlit_bokeh_events(
+            copy_button,
+            events="GET_PLAIN_TEXT2",
+            key="get_plain_text2",
+            refresh_on_update=True,
+            override_height=75,
+            debounce_time=0)
     with col2:
         display = st.checkbox('Display the rejection statement')
         if display:
