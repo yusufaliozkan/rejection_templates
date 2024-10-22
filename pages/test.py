cdf61526031f6abebbd55193d29425321cea5a8f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-22 13:19:51 +0100

    ds

--- pages/test.py
+++ pages/test.py
@@ -25,7 +25,6 @@ column_names_plain = ['rejection reason', 'rejection template plain text']
 df_plain = pd.read_csv('rejection_templates_plain_text.csv', names=column_names_plain)
 df_plain['rejection reason'] = df_plain['rejection reason'].astype(str)
 df_plain_new = df_plain.sort_values(by='rejection reason')
-df_plain_new
 
 df_new = pd.merge(df_new, df_plain_new, on='rejection reason', how='left')
 df_new
