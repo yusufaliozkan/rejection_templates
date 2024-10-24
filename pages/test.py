d7de3b3562cbc8dab52b0b4bdba5a08a11ee77a0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-24 10:14:49 +0100

    sd

--- pages/test.py
+++ pages/test.py
@@ -36,50 +36,92 @@ with tab1:
     clist = df_new['rejection reason'].unique()
     reason = st.selectbox("Select a reason:",clist)
 
-    st.toggle('Plain text')
+    toggle = st.toggle('Plain text')
 
-    col1, col2 = st.columns([1,2])
-    with col1:
+    if toggle:
+        col1, col2 = st.columns([1,2])
+        with col1:
+            df_reason = df_new.loc[df_new['rejection reason']==reason, 'rejection template'].values[0]
+            df_reason_plain_text = df_new.loc[df_new['rejection reason']==reason, 'rejection template plain text'].values[0]
+            text_to_be_copied = df_reason
+            text_to_be_copied_plain = df_reason_plain_text
+            copy_dict = {"content": text_to_be_copied}
+            copy_dict_plain = {"content": text_to_be_copied_plain}
 
-        df_reason = df_new.loc[df_new['rejection reason']==reason, 'rejection template'].values[0]
-        df_reason_plain_text = df_new.loc[df_new['rejection reason']==reason, 'rejection template plain text'].values[0]
-        text_to_be_copied = df_reason
-        text_to_be_copied_plain = df_reason_plain_text
-        copy_dict = {"content": text_to_be_copied}
-        copy_dict_plain = {"content": text_to_be_copied_plain}
+            copy_button = Button(label="Copy HTML template to clipboard")
+            copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
+                navigator.clipboard.writeText(content);
+                """))
 
-        copy_button = Button(label="Copy HTML template to clipboard")
-        copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-            navigator.clipboard.writeText(content);
-            """))
+            no_event = streamlit_bokeh_events(
+                copy_button,
+                events="GET_TEXT",
+                key="get_text",
+                refresh_on_update=True,
+                override_height=75,
+                debounce_time=0)
 
-        no_event = streamlit_bokeh_events(
-            copy_button,
-            events="GET_TEXT",
-            key="get_text",
-            refresh_on_update=True,
-            override_height=75,
-            debounce_time=0)
+            copy_button = Button(label="Copy plain text template to clipboard")
+            copy_button.js_on_event("button_click", CustomJS(args=copy_dict_plain, code="""
+                navigator.clipboard.writeText(content);
+                """))
 
-        copy_button = Button(label="Copy plain text template to clipboard")
-        copy_button.js_on_event("button_click", CustomJS(args=copy_dict_plain, code="""
-            navigator.clipboard.writeText(content);
-            """))
+            no_event = streamlit_bokeh_events(
+                copy_button,
+                events="GET_PLAIN_TEXT",
+                key="get_plain_text",
+                refresh_on_update=True,
+                override_height=75,
+                debounce_time=0)
+        with col2:
+            with st.expander('View template (' + reason+')', expanded=False):
+                components.html(df_reason, height=800, scrolling=True)
+            with st.expander('View template in HTML format (' + reason+')'):
+                st.code(df_reason)
+            with st.expander('View template in plain text (' +reason+')'):
+                st.write(df_reason_plain_text)
+    else:
+        col1, col2 = st.columns([1,2])
+        with col1:
+            df_reason = df_new.loc[df_new['rejection reason']==reason, 'rejection template'].values[0]
+            df_reason_plain_text = df_new.loc[df_new['rejection reason']==reason, 'rejection template plain text'].values[0]
+            text_to_be_copied = df_reason
+            text_to_be_copied_plain = df_reason_plain_text
+            copy_dict = {"content": text_to_be_copied}
+            copy_dict_plain = {"content": text_to_be_copied_plain}
 
-        no_event = streamlit_bokeh_events(
-            copy_button,
-            events="GET_PLAIN_TEXT",
-            key="get_plain_text",
-            refresh_on_update=True,
-            override_height=75,
-            debounce_time=0)
-    with col2:
-        with st.expander('View template (' + reason+')', expanded=False):
-            components.html(df_reason, height=800, scrolling=True)
-        with st.expander('View template in HTML format (' + reason+')'):
-            st.code(df_reason)
-        with st.expander('View template in plain text (' +reason+')'):
-            st.write(df_reason_plain_text)
+            copy_button = Button(label="Copy HTML template to clipboard")
+            copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
+                navigator.clipboard.writeText(content);
+                """))
+
+            no_event = streamlit_bokeh_events(
+                copy_button,
+                events="GET_TEXT",
+                key="get_text",
+                refresh_on_update=True,
+                override_height=75,
+                debounce_time=0)
+
+            copy_button = Button(label="Copy plain text template to clipboard")
+            copy_button.js_on_event("button_click", CustomJS(args=copy_dict_plain, code="""
+                navigator.clipboard.writeText(content);
+                """))
+
+            no_event = streamlit_bokeh_events(
+                copy_button,
+                events="GET_PLAIN_TEXT",
+                key="get_plain_text",
+                refresh_on_update=True,
+                override_height=75,
+                debounce_time=0)
+        with col2:
+            with st.expander('View template (' + reason+')', expanded=False):
+                components.html(df_reason, height=800, scrolling=True)
+            with st.expander('View template in HTML format (' + reason+')'):
+                st.code(df_reason)
+            with st.expander('View template in plain text (' +reason+')'):
+                st.write(df_reason_plain_text)
 
     st.divider()
     col1, col2 = st.columns([1,2])
